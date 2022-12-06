afe1a25b3bc80c889372c925b6be2fd5d98af609
rpiwko <radoslaw.piwko@gmail.com>
2022-12-06 21:10:33 +0100

    Logging setup

---
+++ bin/get_prices.py
@@ -0,0 +1,33 @@
+"""
+Main script to download latest prices
+"""
+
+
+import logging
+from pathlib import Path
+from datetime import datetime
+
+
+print("Script started...")
+
+# Logger elements
+log_formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
+root_logger = logging.getLogger()
+log_files_dir = "../logs"
+log_file_name = Path(__file__).stem + "_" + datetime.now().strftime("%Y%m%d_%H%M%S")
+
+logging.debug("") # warm up to initialize logger handlers
+default_console_handler = root_logger.handlers[0]
+log_file_handler = logging.FileHandler(Path(log_files_dir, log_file_name).with_suffix(".log"))
+log_file_handler.setFormatter(log_formatter)
+root_logger.addHandler(log_file_handler)
+root_logger.setLevel(logging.INFO)
+root_logger.removeHandler(default_console_handler)  # do not propagate logs to console
+
+logging.info("****************************************************************")
+logging.info("*************************** Starting ***************************")
+logging.info("")
+
+
+
+
