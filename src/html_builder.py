d95e77cd8029f1d8cb4a0740676985cc9f1b5d37
rpiwko <radoslaw.piwko@gmail.com>
2023-01-18 18:09:33 +0100

    Setting color on prices and availability cells

--- src/html_builder.py
+++ src/html_builder.py
@@ -6,6 +6,10 @@ Module for building HTML page
 import logging
 
 
+good_value_class = "good-value"
+bad_value_class = "bad-value"
+
+
 def build_table_from_objects(objects_list):
     html_table = "<table>\n"
 
@@ -24,9 +28,9 @@ def build_table_from_objects(objects_list):
         for offer in item["offers"]:
             url = offer['url']
             html_table += f"<td><a href='{url}'>{_get_short_url(url)}</a></td>"
-            html_table += f"<td>{offer['isAvailable']}</td>"
-            html_table += f"<td>{offer['latestPrice']}</td>"
-            html_table += f"<td>{offer['lowestPrice']}</td></tr>\n"
+            html_table += get_is_available_cell(offer)
+            html_table += get_cells_with_prices(offer)
+            html_table += "</tr>\n"
 
     html_table += "</table>"
     logging.info("html_table=\n" + html_table)
@@ -41,6 +45,27 @@ def put_table_into_html_page(table, input_file_path, marker, output_file_path):
     html_page = html_page.replace(marker, table)
     with open(output_file_path, "w") as output_file:
         output_file.write(html_page)
+
+
+def get_is_available_cell(offer):
+    cell = ""
+    if offer['isAvailable']:
+        cell = f"<td class=\"{good_value_class}\">Yes</td>"
+    else:
+        cell = f"<td class=\"{bad_value_class}\">No</td>"
+    return cell
+
+def get_cells_with_prices(offer):
+    cell = f"<td>{offer['latestPrice']}</td>"
+    cell += f"<td>{offer['lowestPrice']}</td>"
+    try:
+        if offer["isAvailable"] and float(offer["latestPrice"]) == float(offer["lowestPrice"]):
+            cell = cell.replace("<td>", f"<td class=\"{good_value_class}\">")
+        if float(offer["latestPrice"]) > float(offer["lowestPrice"]):
+            cell = cell.replace("<td>", f"<td class=\"{bad_value_class}\">")
+    except ValueError:
+        pass
+    return cell
 
 
 def _get_short_url(url):
