2df96b951c0884729cedfd6ad30dc647e9dd09a2
rpiwko <radoslaw.piwko@gmail.com>
2023-01-18 15:13:26 +0100

    Support for ebookpoint.pl

---
+++ src/shop_watcher/shops/ebookpoint.py
@@ -0,0 +1,32 @@
+"""
+Module to find prices on ebookpoint.pl
+"""
+
+
+import logging
+from .base_static_page_shop import base_static_page_shop
+
+
+class ebookpoint(base_static_page_shop):
+
+    def get_supported_domain(self):
+        return "ebookpoint.pl"
+
+
+    def _get_price_and_availability_from_html(self, html):
+        price_string = None
+        is_available = None
+        if html:
+            is_available = self._is_available(html)
+            logging.info("is_available=" + str(is_available))
+            # Price is still shown even when item is not avilable
+            price_tag = html.find_all(id="cena_e")
+            assert len(price_tag) == 1, f"Expected one tag with id='cena_e' but getting {len(price_tag)}"
+            price_string = price_tag[0].get_text()
+            logging.info("price_string=" + price_string)
+        return price_string, is_available
+
+
+    def _is_available(self, html):
+        not_available_tag = html.find_all(class_="tag-navailable")
+        return len(not_available_tag) == 0
