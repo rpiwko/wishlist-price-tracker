dd59864817ce400af956415a1758b88d561db077
rpiwko <radoslaw.piwko@gmail.com>
2023-01-25 18:08:51 +0100

    Preorders for gildia.pl

--- src/shop_watcher/shops/gildia.py
+++ src/shop_watcher/shops/gildia.py
@@ -19,10 +19,13 @@ class gildia(base_static_page_shop):
         if html:
             is_available = self._is_available(html)
             logging.info("is_available=" + str(is_available))
-            # If not is_available, then price is not shown so skip further checking
+            # The is_available is False for:
+            # 1. Preorder: the price is shown
+            # 2. Retired: price is hidden
+            price_tag = html.find_all("span", class_="current-price")
             if is_available:
-                price_tag = html.find_all("span", class_="current-price")
                 assert len(price_tag) == 1, f"Expected one price tag but getting {len(price_tag)}"
+            if len(price_tag) > 0:
                 price_string = price_tag[0].text
                 logging.info("price_string=" + price_string)
         return price_string, is_available
@@ -31,7 +34,8 @@ class gildia(base_static_page_shop):
     def _is_available(self, html):
         product_info_panels = html.find_all("div", class_="product-page-description")
         for product_info_panel in product_info_panels:
-            availability_info = product_info_panel.find("p", class_="light-font")
-            if availability_info and availability_info.text == "Produkt jest aktualnie niedostępny":
+            prod_not_available_info = product_info_panel.find("a", class_="message-on-product-av")
+            if prod_not_available_info and prod_not_available_info.text.strip() == \
+                    "Kliknij tutaj, jeśli chcesz otrzymać maila, gdy produkt się ukaże.":
                 return False
         return True
