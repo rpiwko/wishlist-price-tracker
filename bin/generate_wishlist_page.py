a8fd25e40e78ed9df6f55e95db03d890e27dc3aa
rpiwko <radoslaw.piwko@gmail.com>
2023-06-02 21:47:18 +0200

    Change default output dir

--- bin/generate_wishlist_page.py
+++ bin/generate_wishlist_page.py
@@ -14,7 +14,8 @@ import html_builder
 
 print("Script started...")
 
-log_files_dir = Path(__file__).parent.joinpath("../logs")
+bin_dir_path = Path(__file__).parent
+log_files_dir = bin_dir_path.joinpath("../logs")
 initialize_logging(log_files_dir, __file__)
 
 logging.info("****************************************************************")
@@ -28,6 +29,6 @@ html_category_selector = html_builder.build_category_selector(wishlist_items)
 html_wishlist_items_table = html_builder.build_wishlist_items_table(wishlist_items)
 
 template_file = Path.joinpath(etc_dir_path, "page_template.html")
-output_file = Path.joinpath(Path.home(), "Desktop/my_wishlist.html")
+output_file = Path.joinpath(bin_dir_path.joinpath("../out/my_wishlist.html"))
 html_builder.put_element_into_template(html_category_selector, template_file, "{{CATEGORIES_SELECTOR}}", output_file)
 html_builder.put_element_into_template(html_wishlist_items_table, output_file, "{{WISHLIST_ITEMS_TABLE}}", output_file)
