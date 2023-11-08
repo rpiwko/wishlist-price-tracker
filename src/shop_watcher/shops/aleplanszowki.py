3b868de49797201e258a7d828d1f48030107709e
rpiwko <radoslaw.piwko@gmail.com>
2023-11-08 19:40:25 +0100

    Fix getting prices from aleplanszowki.pl

--- src/shop_watcher/shops/aleplanszowki.py
+++ src/shop_watcher/shops/aleplanszowki.py
@@ -12,7 +12,6 @@ class aleplanszowki(base_static_page_shop):
     def get_supported_domain(self):
         return "aleplanszowki.pl"
 
-
     def _get_price_and_availability_from_html(self, html):
         price_string = None
         is_available = None
@@ -20,19 +19,19 @@ class aleplanszowki(base_static_page_shop):
             is_available = self._is_available(html)
             logging.info("is_available=" + str(is_available))
             # Price is still shown even when item is not available
-            price_tag = html.find_all("span", id="our_price_display")
+            price_tag = html.find_all("span", class_="current-price-display")
             assert len(price_tag) == 1, f"Expected one price tag but getting {len(price_tag)}"
             price_string = price_tag[0].text
             logging.info("price_string=" + price_string)
         return price_string, is_available
 
-
     def _is_available(self, html):
-        product_status_tag = html.find_all("div", id="availability_statut")
-        assert len(product_status_tag) == 1, f"Expected one item status tag but getting {len(product_status_tag)}"
-        inner_prod_status_tag = product_status_tag[0].find_all("span", id="availability_value")
-        assert len(inner_prod_status_tag) == 1, f"Expected one item status tag but getting {len(inner_prod_status_tag)}"
-        if inner_prod_status_tag[0].text == "Ten produkt nie jest już dostępny":
+        inform_when_available_btn = html.find_all("button", class_="js-mailalert-add")
+        assert len(inform_when_available_btn) <= 1, \
+            f"Expected one or no item status tag but getting {len(inform_when_available_btn)}"
+        if len(inform_when_available_btn) == 1:
+            assert inform_when_available_btn[0].text.strip() == "Powiadom o dostępności", \
+                f"Unexpected button text: {inform_when_available_btn[0].text.strip()}"
             return False
         else:
             return True
