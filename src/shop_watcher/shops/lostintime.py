c75d66bb638c2dcf54cb95ae4d57e223c7970bb1
rpiwko <radoslaw.piwko@gmail.com>
2024-06-25 13:19:09 +0200

    Handle no promo on lostintime.pl

--- src/shop_watcher/shops/lostintime.py
+++ src/shop_watcher/shops/lostintime.py
@@ -19,9 +19,15 @@ class lostintime(base_static_page_shop):
             offer_panel = html.find_all("div", class_="entry-summary")
             assert len(offer_panel) == 1, f"Expected one offer panel but getting {len(offer_panel)}"
             is_available = self._is_available(offer_panel[0])
-            price_tag = offer_panel[0].find("ins").find_all("span", class_="woocommerce-Price-amount")
+            price_tag = offer_panel[0].find_all("p", class_="price")
             assert len(price_tag) == 1, f"Expected one price tag but getting {len(price_tag)}"
-            price_string = price_tag[0].text
+            promo_price_tag = price_tag[0].find_all("ins")
+            if promo_price_tag:
+                actual_price_tag = promo_price_tag[0].find_all("span", class_="woocommerce-Price-amount")
+            else:
+                actual_price_tag = price_tag[0].find_all("span", class_="woocommerce-Price-amount")
+            assert len(actual_price_tag) == 1, f"Expected one actual price tag but getting {len(actual_price_tag)}"
+            price_string = actual_price_tag[0].text
             logging.info("price_string=" + price_string)
         return price_string, is_available
 
