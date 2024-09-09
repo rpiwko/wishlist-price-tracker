8b1b77a34df24d3a5d4c76f753f70d85343c0063
rpiwko <radoslaw.piwko@gmail.com>
2024-09-09 13:50:35 +0200

    Adjusting to updated Vesper layout

--- src/shop_watcher/shops/vesper.py
+++ src/shop_watcher/shops/vesper.py
@@ -17,24 +17,21 @@ class vesper(base_static_page_shop):
         price_string = None
         is_available = None
         if html:
-            is_available = self._is_available(html)
+            add_to_cart_field = html.find_all("div", class_="pole_dodawania_do_koszyka")
+            assert len(add_to_cart_field) == 1, f"Expected one add to cart field but getting {len(add_to_cart_field)}"
+            is_available = self._is_available(add_to_cart_field)
             logging.info("is_available=" + str(is_available))
             # Price is still shown even when item is not available
-            price_tag = html.find_all("span", id="our_price_display", attrs={"class": "price", "itemprop": "price"})
+            price_tag = html.find_all("span", class_="current-price-value")
             assert len(price_tag) == 1, f"Expected one price tag but getting {len(price_tag)}"
-            price_string = price_tag[0].get_text()
+            price_string = price_tag[0].get("content")
             logging.info("price_string=" + price_string)
         return price_string, is_available
 
 
     def _is_available(self, html):
-        add_to_cart_btn = html.find("p", id="add_to_cart")
-        before_release_info = html.find("div", id="short_description_block")
-        before_release_info_text = ""
-        if before_release_info:
-            before_release_info_text = before_release_info.find(lambda tag:tag.name=="span" and "Premiera:" in tag.text)
-        if before_release_info_text or not add_to_cart_btn:
+        not_available = len(html[0].find_all("i", call_ = "product-unavailable")) > 0
+        if not_available:
             return False
-        if add_to_cart_btn:
+        else:
             return True
-        raise ValueError("Unable to determine item availability")
